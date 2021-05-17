575222e175beb35ab93455f21908b21538f645fb
Warren Simpson <warren.simpson34@gmail.com>
2021-05-17 14:49:22 -0400

    Made some changes.

--- src/views/SignUpView.js
+++ src/views/SignUpView.js
@@ -5,8 +5,45 @@ const SignUpView = (props) => {
   return (
     <>
       <div class="background2">
-        <div class="container1">
-          
+        <div class="container2">
+          <div class="cell1">
+            <label>Firstname</label>
+            <input class="l1" value={props.state.firstname} name="firstname" onChange={(e) => props.handleInputChange(e)}></input>
+          </div>
+
+          <div class="cell1">
+            <label>Lastname</label>
+            <input class="l1" value={props.state.lastname} name="lastname" onChange={(e) => props.handleInputChange(e)}></input>
+          </div>
+
+          <div class="cell1">
+            <label>Email</label>
+            <input class="l1" value={props.state.email} name="email" onChange={(e) => props.handleInputChange(e)}></input>
+          </div>
+
+          <div class="cell1">
+            <label>Password</label>
+            <input class="l1" value={props.state.password} name="password" onChange={(e) => props.handleInputChange(e)}></input>
+          </div>
+
+          <div class="cell1">
+            <label>Card Number</label>
+            <input class="l1" value={props.state.email} name="card_number" onChange={(e) => props.handleInputChange(e)}></input>
+          </div>
+
+          <div class="cell1">
+            <label>Exp Date</label>
+            <input class="l1" value={props.state.password} name="exp_date" onChange={(e) => props.handleInputChange(e)}></input>
+          </div>
+
+          <div class="cell1">
+            <label>CVC</label>
+            <input class="l1" value={props.state.password} name="cvc" onChange={(e) => props.handleInputChange(e)}></input>
+          </div>
+
+          <button class="signupButton" onClick={(e) => props.handleSignUp(e)}>
+            Sign up
+          </button>
         </div>
       </div>
     </>
